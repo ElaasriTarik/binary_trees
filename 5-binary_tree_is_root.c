948d1a7d5c7474bc4861eb512be3b9f117098315
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 15:28:20 +0100

    fixed 3

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,10 +8,8 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
 
-	if (node->parent == NULL && (node->left != NULL || node->right != NULL))
+	if (node->parent == NULL && node != NULL)
 		return (1);
 
 	return (0);
