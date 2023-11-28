59708144663919731fcbc93fd7ed5c848608c92c
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 19:58:48 +0100

    balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_balance - function inorder
+ *@tree: tree
+ *Return: balance
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+	int height_left = 0;
+	int height_right = 0;
+
+	if (tree != NULL)
+	{
+		height_left = !tree->left ? 0 : binary_tree_balance(tree->left) + 1;
+		height_right = !tree->right ? 0 : binary_tree_balance(tree->right) + 1;
+		balance = height_left - height_right;
+		return (balance);
+	}
+	return (0);
+}
