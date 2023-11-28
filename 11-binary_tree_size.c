f4b3995b84d12b390eebcb1cd771a5d47473b51b
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 18:57:13 +0100

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_size - function inorder
+ *@tree: tree
+ *@func: cunt to print the value
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t s = 1;
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree != NULL)
+	{
+		left = !tree->left ? 0 : binary_tree_size(tree->left);
+		right = !tree->right ? 0 : binary_tree_size(tree->right);
+		s += left + right;
+		return (s);
+	}
+	return (0);
+}
