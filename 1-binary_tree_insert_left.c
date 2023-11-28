eea256914c9b080951faa6f439e3d495f714ac22
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 13:16:55 +0100

    first batch

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+  binary_tree_t *node = malloc(sizeof(binary_tree_t));
+  if (node == NULL)
+    return (NULL);
+
+  node->parent = parent;
+  node->left = NULL;
+  node->right = NULL;
+  node->n = value;
+
+  if (parent->left != NULL)
+    {
+      parent->left->parent = node;
+      node->left = parent->left;
+      parent->left = node;
+    }
+  else
+    {
+      parent->left = node;
+    }
+
+  return (node);
+}
