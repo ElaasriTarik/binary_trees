1d44e10c5545f1d81b5e20cf2d674f28511c1389
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 20:07:35 +0100

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,10 +14,34 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree != NULL)
 	{
-		height_left = !tree->left ? 0 : binary_tree_balance(tree->left) + 1;
-		height_right = !tree->right ? 0 : binary_tree_balance(tree->right) + 1;
+		height_left = !tree->left ? 0 : binary_tree_height(tree->left) + 1;
+		height_right = !tree->right ? 0 : binary_tree_height(tree->right) + 1;
+
 		balance = height_left - height_right;
 		return (balance);
 	}
 	return (0);
+}
+
+/**
+ * binary_tree_height - Calculate the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = !tree->left ? 0 : binary_tree_height(tree->left) + 1;
+	height_right = !tree->right ? 0 : binary_tree_height(tree->right) + 1;
+
+	if (height_right > height_left)
+		return (height_right);
+	else
+		return (height_left);
 }
