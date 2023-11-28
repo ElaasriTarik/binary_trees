7964dd69fba767af776b8b1ff320f790171f821b
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-28 17:16:44 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left;
-	size_t height_right;
+	size_t height_left = 0;
+	size_t height_right = 0;
 	if (tree == NULL)
 		return (0);
 
