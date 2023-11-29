22b3d18e49aaa0d8e397cf94c62972dfff28dc6f
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-30 00:05:11 +0100

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,19 +11,17 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *u_left;
 	binary_tree_t *u_right;
 
-	if (node != NULL && node->parent->parent != NULL)
-	{
-		u_left = node->parent->parent->left;
-		u_right = node->parent->parent->right;
-		if (u_right != NULL && u_left != NULL)
-		{
-			if (u_left == node->parent)
-			{
-				return (u_right);
-			}
-			return (u_left);
-		}
+	if (node == NULL || node->parent->parent == NULL)
 		return (NULL);
-	}
-	return (NULL);
+
+	u_left = node->parent->parent->left;
+	u_right = node->parent->parent->right;
+
+	if (u_right == NULL || u_left == NULL)
+		return (NULL);
+
+	if (u_left == node->parent)
+		return (u_right);
+
+	return (u_left);
 }
