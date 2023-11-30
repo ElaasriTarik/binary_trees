d2988ac2ff6b4e7761ed3269f1a875c02c5cf616
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-30 22:32:01 +0100

    task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,72 @@
+#include "binary_trees.h"
+
+/**
+ *binary_trees_ancestor - finds the sibling of a node
+ *@first: node
+ *@second: second node
+ *Return: returns the least common ancestor
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
+{
+	size_t c1;
+	size_t c2;
+	int x = 0;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	c1 = binary_tree_depth(first);
+	c2 = binary_tree_depth(second);
+	x = c1 - c2;
+	if (x > 0)
+	{
+		while (x > 0)
+		{
+			first = first->parent;
+			x--;
+		}
+		while (first && second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			first = first->parent;
+			second = second->parent;
+		}
+	}
+	if (x <= 0)
+	{
+		while (x < 0)
+		{
+			second = second->parent;
+			x++;
+		}
+		while (first && second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)second);
+			first = first->parent;
+			second = second->parent;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_depth - Calculate the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	depth = !tree->parent ? 0 : binary_tree_depth(tree->parent) + 1;
+
+	return (depth);
+}
